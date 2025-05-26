fecaf4dd4f9a6c1aa7365f87981a769bd24ccc5f
fistandatilus <nikitka236@mail.ru>
2025-05-26 22:24:28 +0300

    added data and params containers

---
+++ data.h
@@ -0,0 +1,52 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include<memory>
+
+enum class rho_type
+{
+    lin1,
+    lin10,
+    lin100,
+    gamma,
+};
+
+class data
+{
+public:
+    std::unique_ptr<double []> g{};
+    std::unique_ptr<double []> v1{};
+    std::unique_ptr<double []> v2{};
+
+    unsigned int size = 0;
+
+    data(const unsigned int size) : size(size)
+    {
+        g  = std::make_unique<double []>(size);
+        v1 = std::make_unique<double []>(size);
+        v2 = std::make_unique<double []>(size);
+    }
+};
+
+struct problem_params
+{
+    double mu = 0;
+    rho_type type = rho_type::lin1;
+
+    problem_params(double mu, rho_type type) : mu(mu), type(type)
+    {};
+};
+
+struct scheme_params
+{
+    unsigned int N = 0;
+    unsigned int M = 0;
+    double tau = 0;
+    double h = 0;
+    double T = 0;
+
+    scheme_params(unsigned int N, unsigned int M, double T) : N(N), M(M), T(T), tau(1. / N), h(1. / M)
+    {};
+};
+
+#endif //DATA_H
